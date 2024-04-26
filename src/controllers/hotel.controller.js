18ebb97c1c4ff0a0b6ac26fb4df37fb1fb577d64
rparedes-2022088 <rparedes-2022088@kinal.edu.gt>
2024-04-26 17:15:44 -0600

    Se ha terminado el crud de hoteles

--- src/controllers/hotel.controller.js
+++ src/controllers/hotel.controller.js
@@ -16,9 +16,40 @@ export const newHotel = async(req, res) => {
 
 export const updateHotel = async(req, res) => {
     try{
-
+        let { id } = req.params
+        let data = req.body
+        let updatedHotel = await Hotel.findOneAndUpdate(
+            {_id: id},
+            data,
+            {new: true}
+        )
+        if(!updatedHotel) return res.status(404).send({message: 'Hotel no encontrado, no se ha actualizado'})
+        return res.send({message: 'Hotel actualizado', updatedHotel})
     }catch(err){
         console.error(err)
         return res.status(500).send({message: 'Error al actualizar el hotel'})
+    }
+}
+
+export const deleteHotel = async(req, res) => {
+    try{
+        let { id } = req.params
+        let deletedHotel = await Hotel.findOneAndDelete({_id: id})
+        if(!deletedHotel) return res.status(404).send({message: 'Hotel no encontrado, no se ha actualizado'})
+        return res.send({message: `El hotel ${deletedHotel.name} ha sido eliminado`})
+    }catch(err){
+        console.error(err)
+        return res.status(500).send({message: 'Error al eliminar el hotel'})
+    }
+}
+
+export const viewHotels = async(req, res) => {
+    try{
+        let hotelsFound = await Hotel.find()
+        if(!hotelsFound) return res.status(404).send({message: 'No se han encontrado hoteles'})
+        return res.send({hotelsFound})
+    }catch(err){
+        console.error(err)
+        return res.status(500).send({message: 'Error al visualizar los hoteles'})
     }
 }
--- src/routes/hotel.routes.js
+++ src/routes/hotel.routes.js
@@ -1,11 +1,14 @@
 'use strict'
 
 import { Router } from 'express'
-import { newHotel } from '../controllers/hotel.controller.js'
+import { newHotel, updateHotel, deleteHotel, viewHotels } from '../controllers/hotel.controller.js'
 import { validateJwt } from '../middlewares/validate-jwt.js'
 
 const api = Router()
 
 api.post('/newHotel', newHotel)
+api.put('/update', updateHotel)
+api.delete('/delete', deleteHotel)
+api.get('/get', viewHotels)
 
 export default api
