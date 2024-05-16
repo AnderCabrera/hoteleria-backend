88d6dd20a6eebd283a534ba0a467d33d0d458930
rparedes-2022088 <rparedes-2022088@kinal.edu.gt>
2024-05-16 15:37:06 -0600

    Función que retorna las fechas de las reservaciones de esa habitación

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -18,3 +18,26 @@ export const newBooking = async (req, res) => {
       .send({ message: 'Error al agregar la habitación a su cuenta' });
   }
 };
+
+export const getDates = async (req, res) => {
+  try {
+    let { idRoom } = req.params;
+    let foundedDates = await Booking.find({ room: idRoom });
+    if (!foundedDates)
+      return res
+        .status(404)
+        .send({ message: 'Esta habitación no ha sido reservada aún' });
+    let dates = foundedDates.map((booking) => {
+      return {
+        date_start: booking.date_start,
+        date_end: booking.date_end,
+      };
+    });
+    return res.status(200).send(dates);
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .send({ message: 'Error al obtener las fechas de reservación' });
+  }
+};
